0eee527d18545efc6231b79edb45e82dc056caee
Svetlana <strel_sv@bk.ru>
2022-04-14 16:14:48 +0300

    add example 004 Array Library corrected

--- example004_ArrayLibrary/Program.cs
+++ example004_ArrayLibrary/Program.cs
@@ -25,7 +25,7 @@ int IndexOf(int[] collection, int find)
 {
     int count = collection.Length;
     int index = 0;
-    int position = 0;
+    int position = -1;
     while(index < count)
     {
         if(collection[index] == find)
@@ -42,10 +42,9 @@ int[] array = new int[10];
 
 FillArray(array);
 array[1] = 4;
-array[6] = 4;
 
 PrintArray(array);
 Console.WriteLine();
 
-int pos = IndexOf(array, 3);
+int pos = IndexOf(array, 1);
 Console.WriteLine(pos);
